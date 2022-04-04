e397f6e9706148aa74387be816782e297ca78adc
yuliya.klimava <yuliya.klimava@leverx.com>
2022-04-04 17:52:09 +0300

    Add task's queue

--- task-4/courses/tasks.py
+++ task-4/courses/tasks.py
@@ -1,19 +1,18 @@
-import os
+from celery import Task
+from .celery_main import app
 
-from celery import Celery
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'courses.settings')
+# periodic task
+class PeriodicTask(Task):
+    def send_email(self, email):
+        print(f'Message is sent to {email}')
 
-app = Celery('courses', broker='CELERY_BROKER_URL')
-app.config_from_object('django.conf:settings', namespace='CELERY')
 
-app.autodiscover_tasks()
+# static task
+class StaticTask(Task):
+    def run(self, *args, **kwargs):
+        print('Static task is completed')
 
-#schedule
-app.conf.beat_schedule = {
-    'every-10-seconds': {
-        'task': 'notifications.tasks.send_email',
-        'schedule': 10,
-        'args': ('klimava27@gmail.com',)
-    }
-}
+
+app.register_task(StaticTask)
+app.register_task(PeriodicTask)
